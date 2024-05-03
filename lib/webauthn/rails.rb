f75febd4eae324ea0828d0a2106cfe4e0dbfdfa6
Santiago Rodriguez <santiago.rodriguez@cedarcode.com>
2024-05-03 17:19:44 -0300

    feat: add `current_user` logic
    Include it in the main app controllers so that it can be used widely
    across the app.

--- lib/webauthn/rails.rb
+++ lib/webauthn/rails.rb
@@ -4,6 +4,10 @@ require "webauthn/rails/engine"
 module Webauthn
   module Rails
 
+    module Controllers
+      autoload :Helpers, 'webauthn/rails/controllers/helpers'
+    end
+
     mattr_accessor :webauthn_origin
 
     def self.configure
