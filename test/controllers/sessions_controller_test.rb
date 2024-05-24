ecce766c979f4e8ff2b737434ee58a7099a13b9f
Santiago Rodriguez <santiago.rodriguez@cedarcode.com>
2024-05-24 16:44:07 -0300

    test: add simple controller tests

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test "should initiate registration successfully" do
+    User.create!(username: "alice")
+
+    post webauthn_rails.session_url, params: { session: { username: "alice" }, format: :turbo_stream }
+
+    assert_response :success
+  end
+
+  test "should return error if creating session with inexisting username" do
+    post webauthn_rails.session_url, params: { session: { username: "alice" }, format: :turbo_stream }
+
+    assert_response :unprocessable_entity
+    assert_equal ["Username doesn't exist"], JSON.parse(response.body)["errors"]
+  end
+
+  test "should return error if creating session with blank username" do
+    post webauthn_rails.session_url, params: { session: { username: "" }, format: :turbo_stream }
+
+    assert_response :unprocessable_entity
+    assert_equal ["Username doesn't exist"], JSON.parse(response.body)["errors"]
+  end
+end
