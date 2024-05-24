0e12516363f8b84e79dd482c04e1ab6edc5e975c
Santiago Rodriguez <santiago.rodriguez@cedarcode.com>
2024-05-24 20:26:59 -0300

    test: add basic system tests

---
+++ test/system/add_credential_test.rb
@@ -0,0 +1,40 @@
+require "application_system_test_case"
+require "webauthn/fake_client"
+
+class AddCredentialTest < ApplicationSystemTestCase
+  test "add credentials" do
+    fake_origin = 'http://localhost:3030'
+    fake_client = WebAuthn::FakeClient.new(fake_origin, encoding: false)
+    fixed_challenge = SecureRandom.random_bytes(32)
+
+    visit webauthn_rails.new_registration_path
+
+    fake_credentials = fake_client.create(challenge: fixed_challenge, user_verified: true)
+    stub_create(fake_credentials)
+
+    fill_in "registration_username", with: "User1"
+    fill_in "Security Key nickname", with: "USB key"
+
+    WebAuthn::PublicKeyCredential::CreationOptions.stub_any_instance :raw_challenge, fixed_challenge do
+      click_on "Sign up"
+      # wait for async response
+      assert_text "Your Security Keys"
+    end
+
+    click_on "Add credential"
+
+    fake_credentials = fake_client.create(challenge: fixed_challenge, user_verified: true)
+    stub_create(fake_credentials)
+
+    fill_in("Security Key nickname", with: "Touch ID")
+
+    WebAuthn::PublicKeyCredential::CreationOptions.stub_any_instance :raw_challenge, fixed_challenge do
+      click_on "Add Security Key"
+      # wait for async response
+      assert_text 'Touch ID'
+    end
+
+    assert_current_path "/"
+    assert_text 'USB key'
+  end
+end
