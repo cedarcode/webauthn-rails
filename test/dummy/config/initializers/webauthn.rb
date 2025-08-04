564b0e2145f5df7e5b4922383344038b9fa0e879
rafaella-martino <rafaella.martino@cedarcode.com>
2025-08-04 11:18:20 -0300

    feat: change origin for `allowed_origins` in webauthn config

--- test/dummy/config/initializers/webauthn.rb
+++ test/dummy/config/initializers/webauthn.rb
@@ -1,7 +1,7 @@
 WebAuthn.configure do |config|
   # This value needs to match `window.location.origin` evaluated by
   # the User Agent during registration and authentication ceremonies.
-  config.origin = "http://localhost:3030"
+  config.allowed_origins = [ "http://localhost:3030" ]
 
   # Relying Party name for display purposes
   config.rp_name = "WebAuthn Rails Demo App"
