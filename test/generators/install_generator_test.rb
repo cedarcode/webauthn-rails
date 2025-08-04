46c46311137e17ee187fa9bfab077191fd328b81
Nicolas Temciuc <nicotemciuc@gmail.com>
2025-08-04 11:12:17 -0300

    test: add test for generators (#24)

---
+++ test/generators/install_generator_test.rb
@@ -0,0 +1,79 @@
+require "test_helper"
+require "rails/generators/test_case"
+require "generators/webauthn/rails/install_generator"
+
+class InstallGeneratorTest < Rails::Generators::TestCase
+  tests Webauthn::Rails::InstallGenerator
+  destination File.expand_path("../../tmp", __FILE__)
+
+  setup do
+    prepare_destination
+    add_config_folder
+    add_importmap
+    add_routes
+  end
+
+  test "assert all files are properly created when user model does not exist" do
+    run_generator
+
+    assert_file "app/javascript/controllers/webauthn/rails/add_credential_controller.js"
+    assert_file "app/javascript/controllers/webauthn/rails/new_registration_controller.js"
+    assert_file "app/javascript/controllers/webauthn/rails/new_session_controller.js"
+
+    assert_file "config/initializers/webauthn.rb", /WebAuthn.configure/
+
+    assert_file "app/models/user.rb", /has_many :credentials/
+    assert_migration "db/migrate/create_users.rb", /create_table :users/
+    assert_migration "db/migrate/create_webauthn_rails_credentials.rb", /create_table :webauthn_rails_credentials/
+
+    assert_file "config/routes.rb", /mount Webauthn::Rails::Engine/
+
+    assert_file "config/importmap.rb", /pin "@github\/webauthn-json"/
+  end
+
+  test "assert all files are properly created when user model already exists" do
+    add_user_model
+
+    run_generator
+
+    assert_file "app/javascript/controllers/webauthn/rails/add_credential_controller.js"
+    assert_file "app/javascript/controllers/webauthn/rails/new_registration_controller.js"
+    assert_file "app/javascript/controllers/webauthn/rails/new_session_controller.js"
+
+    assert_file "config/initializers/webauthn.rb", /WebAuthn.configure/
+
+    assert_file "app/models/user.rb", /has_many :credentials/
+    assert_migration "db/migrate/add_webauthn_to_users.rb", /change_table :users/
+    assert_migration "db/migrate/create_webauthn_rails_credentials.rb", /create_table :webauthn_rails_credentials/
+
+    assert_file "config/routes.rb", /mount Webauthn::Rails::Engine/
+
+    assert_file "config/importmap.rb", /pin "@github\/webauthn-json"/
+  end
+
+  private
+
+  def add_config_folder
+    FileUtils.mkdir_p(File.join(destination_root, "config"))
+  end
+
+  def add_importmap
+    FileUtils.touch(File.join(destination_root, "config", "importmap.rb"))
+  end
+
+  def add_routes
+    File.write(File.join(destination_root, "config", "routes.rb"), <<~CONTENT)
+      Rails.application.routes.draw do
+      end
+    CONTENT
+  end
+
+  def add_user_model
+    app_folder = FileUtils.mkdir_p(File.join(destination_root, "app"))
+    FileUtils.mkdir_p(File.join(app_folder, "models"))
+    File.write(File.join(destination_root, "app", "models", "user.rb"), <<~CONTENT)
+      class User < ApplicationRecord
+      end
+    CONTENT
+  end
+end
