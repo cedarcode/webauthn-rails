3b378743c2ec318542348415f76e087e1f0807f2
Rafaella Martino <rafaellamiza@gmail.com>
2025-08-04 11:30:33 -0300

    feat: change origin for `allowed_origins` in webauthn config (#27)

--- lib/generators/webauthn/rails/templates/config/initializers/webauthn.rb
+++ lib/generators/webauthn/rails/templates/config/initializers/webauthn.rb
@@ -1,7 +1,7 @@
 WebAuthn.configure do |config|
   # This value needs to match `window.location.origin` evaluated by
   # the User Agent during registration and authentication ceremonies.
-  # config.origin = "https://auth.example.com"
+  # config.allowed_origins = [ "https://auth.example.com" ]
 
   # Relying Party name for display purposes
   # config.rp_name = "Example Inc."
