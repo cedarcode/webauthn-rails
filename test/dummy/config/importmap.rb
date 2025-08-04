97a576a00af28c95ff400ee24a8518d329c3e98a
Joaquin Tomas <joaquintomas2003@gmail.com>
2025-08-04 15:11:17 -0300

    chore(dummy_app): `bin/importmap pin @github/webauthn-json/browser-ponyfill`

--- test/dummy/config/importmap.rb
+++ test/dummy/config/importmap.rb
@@ -5,7 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@github/webauthn-json", to: "https://ga.jspm.io/npm:@github/webauthn-json@2.1.1/dist/esm/webauthn-json.js"
+pin "@github/webauthn-json/browser-ponyfill", to: "@github--webauthn-json--browser-ponyfill.js"
 pin "webauthn-rails/credential", to: "credential.js"
 pin "stub_credentials" if Rails.env.test?
 pin "sinon" # @18.0.0
