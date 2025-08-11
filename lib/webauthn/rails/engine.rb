1ea01e2a8e8aaacfd8b90f430cc1102fecebf3a6
Rafaella Martino <rafaella.martino@cedarcode.com>
2025-08-11 14:01:58 -0300

    fix: remove unused importmap (#35)

--- lib/webauthn/rails/engine.rb
+++ lib/webauthn/rails/engine.rb
@@ -5,12 +5,6 @@ module Webauthn
     class Engine < ::Rails::Engine
       isolate_namespace Webauthn::Rails
 
-      initializer "webautn-rails.assets" do
-        if ::Rails.application.config.respond_to?(:assets)
-          ::Rails.application.config.assets.precompile += %w[ credential.js ]
-        end
-      end
-
       initializer "webauthn-rails.helpers" do
         ActiveSupport.on_load(:action_controller) do
           include Webauthn::Rails::Controllers::Helpers
