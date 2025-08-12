d858494804c0199e714085c5ed515401f8ef3191
Joaquin Tomas <joaquintomas2003@gmail.com>
2025-08-12 12:34:14 -0300

    chore: remove `manifest.js` from dummy app

--- test/dummy/app/assets/config/manifest.js
+++
@@ -1,5 +0,0 @@
-//= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link webauthn_rails_manifest.js
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
