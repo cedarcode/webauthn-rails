c980a3c0d92b043eed2e0090f866f40ac79e77bf
rafaella-martino <rafaella.martino@cedarcode.com>
2025-08-20 15:07:08 -0300

    chore: Do not load `fixtures` from removed engine

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,11 +3,3 @@ ENV["RAILS_ENV"] = "test"
 
 require "minitest/autorun"
 require "active_record/railtie"
-
-# Load fixtures from the engine
-if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
-  ActiveSupport::TestCase.fixture_paths = [ File.expand_path("fixtures", __dir__) ]
-  ActionDispatch::IntegrationTest.fixture_paths = ActiveSupport::TestCase.fixture_paths
-  ActiveSupport::TestCase.file_fixture_path = File.expand_path("fixtures", __dir__) + "/files"
-  ActiveSupport::TestCase.fixtures :all
-end
