7df158974c0ac1c656a941c7ab99a10e30e368ac
DiegoPisa-Cedar <diego.pisa@cedarcode.com>
2025-08-21 09:27:43 -0300

    chore: remove require of dummy app from  engine test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,8 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
-require_relative "../test/dummy/config/environment"
-require "rails/test_help"
+require "active_record/railtie"
+require "minitest/autorun"
 
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
