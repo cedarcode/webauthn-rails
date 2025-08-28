45f2a11fd58b0158f3569a9ab47c5413b27af117
rafaella-martino <rafaella.martino@cedarcode.com>
2025-08-28 15:43:43 -0300

    test: remove rails app mocking for testing generator

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,6 @@ ENV["RAILS_ENV"] = "test"
 
 require "minitest/autorun"
 require "active_record/railtie"
-require_relative "support/rails_method_mocks"
 
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
