ed7a3d2f3b455d628ef4e2e197e552b46e05f872
rafaella-martino <rafaella.martino@cedarcode.com>
2025-09-08 12:14:02 -0300

    tests: update tests and helpers with feature discoverable credentials

--- lib/generators/test_unit/webauthn_authentication/templates/test/controllers/webauthn_sessions_controller_test.rb
+++ lib/generators/test_unit/webauthn_authentication/templates/test/controllers/webauthn_sessions_controller_test.rb
@@ -1,25 +1,10 @@
 require "test_helper"
 
 class WebauthnSessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should initiate registration successfully" do
-    User.create!(username: "alice")
+  test "should initiate sign_in using passkeys successfully" do
 
-    post get_options_webauthn_session_url, params: { session: { username: "alice" } }
+    post get_options_webauthn_session_url
 
     assert_response :success
-  end
-
-  test "should return error if creating session with inexisting username" do
-    post get_options_webauthn_session_url, params: { session: { username: "alice" } }
-
-    assert_response :unprocessable_entity
-    assert_equal [ "Username doesn't exist" ], JSON.parse(response.body)["errors"]
-  end
-
-  test "should return error if creating session with blank username" do
-    post get_options_webauthn_session_url, params: { session: { username: "" } }
-
-    assert_response :unprocessable_entity
-    assert_equal [ "Username doesn't exist" ], JSON.parse(response.body)["errors"]
   end
 end
