a380b9f3b714fbdb8d53e69f1e5e731a73f85eef
rafaella-martino <rafaella.martino@cedarcode.com>
2025-09-08 12:15:09 -0300

    test: new system test

---
+++ lib/generators/test_unit/webauthn_authentication/templates/test/system/manage_webauthn_credentials_test.rb
@@ -0,0 +1,51 @@
+require "application_system_test_case"
+require_relative "../test_helpers/virtual_authenticator_test_helper"
+require "test_helper"
+
+class ManageWebauthnCredentialsTest < ApplicationSystemTestCase
+  include VirtualAuthenticatorTestHelper
+
+  def setup
+    @user = users(:one)
+    sign_in_as(@user)
+    @authenticator = add_virtual_authenticator
+  end
+
+  def teardown
+    @authenticator.remove!
+  end
+
+  test "add credentials and sign in" do
+    visit root_path
+
+    click_on "Add credential"
+
+    fill_in("Security Key nickname", with: "Touch ID")
+    click_on "Add Security Key"
+
+    assert_text "Security Key registered successfully"
+    assert_selector "span", text: "Touch ID"
+    assert_current_path "/"
+
+    click_on "Sign out"
+    assert_selector("input[type=submit][value='Sign in']")
+
+    click_on "Sign in with Passkey"
+
+    assert_selector "h3", text: "Your Security Keys"
+    assert_current_path "/"
+  end
+
+  private
+
+  def sign_in_as(user)
+    visit new_session_path
+
+    fill_in "email_address", with: user.email_address
+    fill_in "password", with: "password"
+
+    click_on "Sign in"
+
+    assert_selector "h3", text: "Your Security Keys"
+  end
+end
