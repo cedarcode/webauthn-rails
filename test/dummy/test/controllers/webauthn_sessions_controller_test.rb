ede4f1f61fb34856ea2097a3f444fc0560bf5f3b
Joaquin Tomas <joaquintomas2003@gmail.com>
2025-09-17 16:58:07 -0300

    Improve controllers tests

--- test/dummy/test/controllers/webauthn_sessions_controller_test.rb
+++ test/dummy/test/controllers/webauthn_sessions_controller_test.rb
@@ -1,9 +1,90 @@
 require "test_helper"
+require "webauthn/fake_client"
 
 class WebauthnSessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should initiate sign_in using passkeys successfully" do
+  setup do
+    @user = users(:one)
+    @client = WebAuthn::FakeClient.new(WebAuthn.configuration.allowed_origins.first)
+
+    creation_options = WebAuthn::Credential.options_for_create(
+      user: { id: @user.webauthn_id, name: @user.email_address }
+    )
+    create_options = @client.create(challenge: creation_options.challenge)
+    credential = WebAuthn::Credential.from_create(create_options)
+
+    WebauthnCredential.create!(
+      nickname: "My Passkey",
+      user: @user,
+      external_id: credential.id,
+      public_key: credential.public_key,
+      sign_count: 0
+    )
+  end
+
+  test "should return get_options" do
     post get_options_webauthn_session_url
 
     assert_response :success
+    body = JSON.parse(response.body)
+    assert body["challenge"].present?
+    assert body["userVerification"] == "required"
+
+    assert_equal session[:current_authentication][:challenge], body["challenge"]
+  end
+
+  test "should create session with valid credential" do
+    post get_options_webauthn_session_url
+    challenge = session[:current_authentication][:challenge]
+
+    public_key_credential = @client.get(challenge: challenge, user_verified: true)
+
+    post webauthn_session_url, params: {
+      session: {
+        public_key_credential: public_key_credential.to_json
+      }
+    }
+
+    assert_redirected_to root_path
+    assert_nil session[:current_authentication]
+  end
+
+  test "should not create session when there is a Webauthn error" do
+    post get_options_webauthn_session_url
+    challenge = session[:current_authentication][:challenge]
+
+    public_key_credential = @client.get(challenge: challenge, user_verified: false)
+
+    post webauthn_session_url, params: {
+      session: {
+        public_key_credential: public_key_credential.to_json
+      }
+    }
+
+    assert_redirected_to new_session_path
+    assert_match (/Verification failed/), flash[:alert]
+    assert_nil session[:current_authentication]
+  end
+
+  test "should not create session with unrecognized credential" do
+    post get_options_webauthn_session_url
+    challenge = session[:current_authentication][:challenge]
+
+    public_key_credential = @client.get(challenge: challenge, user_verified: true)
+    public_key_credential["id"] = "invalid-id"
+
+    post webauthn_session_url, params: {
+      session: {
+        public_key_credential: public_key_credential.to_json
+      }
+    }
+
+    assert_redirected_to new_session_path
+    assert_equal "Credential not recognized", flash[:alert]
+    assert_nil session[:current_authentication]
+  end
+
+  test "should destroy session" do
+    delete webauthn_session_url
+    assert_redirected_to new_session_path
   end
 end
