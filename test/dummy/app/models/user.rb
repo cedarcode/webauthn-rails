9fe995d255f677110bb7e01443090bc7498ba053
Joaquin Tomas <joaquintomas2003@gmail.com>
2025-09-19 17:12:26 -0300

    Remove constraint to delete passkeys

--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -4,15 +4,9 @@ class User < ApplicationRecord
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
-  CREDENTIAL_MIN_AMOUNT = 1
-
   has_many :webauthn_credentials, dependent: :destroy
 
   after_initialize do
     self.webauthn_id ||= WebAuthn.generate_user_id
-  end
-
-  def can_delete_credentials?
-    webauthn_credentials.size > CREDENTIAL_MIN_AMOUNT
   end
 end
