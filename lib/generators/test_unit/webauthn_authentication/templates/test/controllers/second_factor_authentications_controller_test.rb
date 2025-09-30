7fee1bded4fc2b6316ac2838293e62ee9980f7cb
Joaquin Tomas <joaquintomas2003@gmail.com>
2025-09-30 11:57:24 -0300

    Test for using passkey for 2fa

--- lib/generators/test_unit/webauthn_authentication/templates/test/controllers/second_factor_authentications_controller_test.rb
+++ lib/generators/test_unit/webauthn_authentication/templates/test/controllers/second_factor_authentications_controller_test.rb
@@ -53,6 +53,42 @@ class SecondFactorAuthenticationsControllerTest < ActionDispatch::IntegrationTes
     assert_nil session[:current_authentication]
   end
 
+  test "create with a passkey" do
+    client = WebAuthn::FakeClient.new(WebAuthn.configuration.allowed_origins.first)
+
+    creation_options = WebAuthn::Credential.options_for_create(
+      user: { id: @user.webauthn_id, name: @user.email_address },
+      authenticator_selection: { resident_key: "discouraged", user_verification: "discouraged" }
+    )
+    create_options = client.create(challenge: creation_options.challenge)
+    credential = WebAuthn::Credential.from_create(create_options)
+
+
+    WebauthnCredential.passkey.create!(
+      nickname: "My Security Key",
+      user: @user,
+      external_id: credential.id,
+      public_key: credential.public_key,
+      sign_count: 0
+    )
+
+    post session_path, params: { email_address: @user.email_address, password: "password" }
+
+    post get_options_second_factor_authentication_url
+    challenge = session[:current_authentication][:challenge]
+
+    public_key_credential = client.get(challenge: challenge, user_verified: false)
+
+    post second_factor_authentication_url, params: {
+      session: {
+        public_key_credential: public_key_credential.to_json
+      }
+    }
+
+    assert_redirected_to root_path
+    assert_nil session[:current_authentication]
+  end
+
   test "create with WebAuthn error" do
     post session_path, params: { email_address: @user.email_address, password: "password" }
 
